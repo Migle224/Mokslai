e48da7446aaf7914adb283861870b4cb53054c7b
Migle224 <mpucetaite@gmail.com>
2016-11-03 20:59:26 +0200

    Projektinis, papildoma scena
    pirmas uždavinys su stack ir atminties segmentais

---
+++ projektinis/ComputerArchitechure/Assets/MemoryRegistersController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MemoryRegistersController : MonoBehaviour {
+
+    public Text axValue, bxValue, cxValue, dxValue, csValue, ipValue,
+        ssValue, spValue, bpValue, siValue, diValue, dsValue, esValue;
+    // Use this for initialization
+    void Start () {
+        this.initTask();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    /*Valdymo perdavimas [4]Besąlyginis valdymo perdavimas [3]UŽDAVINYS NR.1
+     * Uždavinio sąlyga:Registras SS=ABCD, registras SP=0002, 
+     * registras BP=AF00, registras CX=0010. 
+     * Kokia bus stekosegmento baito su adresu FFFE reikšmė šešioliktainėje sistemoje, 
+     * įvykdžius komandą:1234 9A EBFE6789call text (1234 yra poslinkis kodo segmente)*/
+    void initTask()
+    {
+        ssValue.text = "ABCD";
+        spValue.text = "0002";
+        bpValue.text = "AF00";
+        cxValue.text = "0010";
+    }
+}
---
+++ projektinis/ComputerArchitechure/Assets/Scripts/MemoryScrollViewController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MemoryScrollViewController : MonoBehaviour {
+
+    public GameObject memoryInfoPrefab;
+
+    GameObject memoryInfo;
+    GameObject content;
+    int buttonAmount = 30;
+    RectTransform rt;
+    float buttonHeight;
+
+    // Use this for initialization
+    void Start () {
+
+        buttonHeight = memoryInfoPrefab.transform.GetChild(0).gameObject.GetComponent<RectTransform>().sizeDelta.y;
+
+        content = this.gameObject.transform.parent.gameObject;
+        content.GetComponent<RectTransform>().sizeDelta = new Vector2(content.GetComponent<RectTransform>().sizeDelta.x, buttonHeight * buttonAmount);
+
+        this.gameObject.transform.position = new Vector2(this.gameObject.GetComponent<RectTransform>().position.x, 
+                                                          content.transform.position.y - content.GetComponent<RectTransform>().sizeDelta.y / buttonHeight + buttonAmount - buttonHeight/2);
+
+        for (int i = 0; i < buttonAmount; i++)
+        {
+            memoryInfo = (GameObject)Instantiate(memoryInfoPrefab, this.gameObject.transform);
+            memoryInfo.GetComponent<RectTransform>().localPosition = new Vector3(0, 0 - buttonHeight * i);
+            memoryInfo.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "test " + i;
+        }
+
+        this.initTask();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void initTask()
+    {
+        Button button;
+        button = memoryInfo.transform.GetChild(0).gameObject.GetComponent<Button>();
+        button.transform.GetChild(0).GetComponent<Text>().text = "FFFF ";
+        button.image.color = Color.green;
+
+    }
+}
