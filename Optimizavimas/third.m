d92343d5e5a05254d0cae2ad2249fbb52ab2f456
Migle224 <mpucetaite@gmail.com>
2016-12-20 01:21:07 +0200

    optimi

--- Optimizavimas/third.m
+++ Optimizavimas/third.m
@@ -1,12 +1,12 @@
 c = [2, -3, 0, -5]';
 A = [ -1, 1, -1, -1;
      2, 4, 0, 0;
-     0, 0, -1, 1];
+     0, 0, 1, -1];
      
-b = [8, 10, 3]';
+b = [8, 10, -3]';
 lb = [0, -Inf, 0, -Inf]';
 ub = [];
-ctype = "SSU";
+ctype = "SSL";
 vartype = "IIII";
 s = 1;
 
@@ -20,19 +20,19 @@ b = [5, 2, 1]';
 [xmin, fmin, status, extra] = glpk (c, A, b, lb, ub, ctype, vartype, s, param)
 
 
-c = [-1, 2, 0, 1, 0]';
-A = [ -1, 2, 0, 1, 0;
-     1, 4, 0, 0, 0;
-     -1, 0, -1, 0, 1,
-     -1, 0, 1, 0, 0];
+c = [8,10,-3]';
+A = [ -1, 2, 0;
+     1, 4, 0;
+     -1, 0, 1,
+     -1, 0, -1];
      
 b = [2, -3, 0, -5]';
-lb = [-Inf, -Inf, -Inf, -Inf, -Inf]';
+lb = [-Inf, -Inf, 0]';
 ub = [];
-ctype = "SSSS";
-vartype = "IIIII";
+ctype = "USUS";
+vartype = "III";
 s = -1;
 
-param.itlim = 100;
+param.itlim = 1000;
 
 [xmin, fmin, status, extra] = glpk (c, A, b, lb, ub, ctype, vartype, s, param)
